44bba293da93026ea4172c5b227f52e3d12dc96d
Pooja <poojsansh@gmail.com>
2023-02-01 18:13:21 +0530

    First commit by pooja(31-01-2023)

--- test/conftest.py
+++ test/conftest.py
@@ -36,7 +36,7 @@ def setup(request):
     driver.close()
 
 
-
+"""
     @pytest.mark.hookwrapper
     def pytest_runtest_makereport(item):
 
@@ -67,3 +67,32 @@ def setup(request):
         #path = '/home/atpl/Pooja/PycharmProjects/PythonSelfFramework/screenshot/'+name
         print(name)
         driver.get_screenshot_as_file(name)
+"""
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + time.ctime() + ".png"
+            mod_string = file_name[5:]
+            file_name = 'screenshot/' + mod_string
+            _capture_screenshot(file_name)
+            """if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+            """
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
